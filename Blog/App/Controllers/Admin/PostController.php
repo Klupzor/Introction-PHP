1db2b7b99f32fb35e87bf6a8686a32a0c55208dd
Klupzor <anloges@hotmail.com>
2018-02-11 20:42:10 -0500

    Se modificó el postcontroller y el blogpost

--- Blog/App/Controllers/Admin/PostController.php
+++ Blog/App/Controllers/Admin/PostController.php
@@ -2,16 +2,11 @@
 namespace APP\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\models\BlogPost;
 
 class PostController extends BaseController {
   public function getIndex(){
-    global $pdo;
-    $query = $pdo->prepare('SELECT * FROM blog_post ORDER BY id DESC');
-    $query->execute();
-
-
-    $blogPost = $query->fetchAll(\PDO::FETCH_ASSOC);
-
+    $blogPost = BlogPost::all();
 
     return $this->render('admin/posts.twig', ['blogPost' => $blogPost]);
   }
@@ -22,14 +17,13 @@ class PostController extends BaseController {
     }
 
     public function postCreate(){
-      global  $pdo;
-      $sql = 'INSERT INTO blog_post(title, content) VALUES (:title, :content)';
-      $query = $pdo->prepare($sql);
-      $result = $query->execute([
+      $blogPost = new BlogPost([
         'title' => $_POST['title'],
         'content' => $_POST['content']
 
       ]);
+      $blogPost->save();
+      $result = true;
 
      return $this->render('admin/insert-post.twig',['result' => $result]);
     }
--- Blog/App/models/BlogPost.php
+++ Blog/App/models/BlogPost.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class BlogPost extends Model {
   protected $table = 'blog_post';
+  protected $fillable = ['title','content'];
 }
 
  ?>
